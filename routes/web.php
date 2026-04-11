6680eaa3a3f6554fbea61782b718b06d7502044f
sofisofs <sofiablanco.c@icloud.com>
2026-04-11 21:17:02 +0200

    [WIP] Rutas basicas para empezar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'PaginaInicio')->name('index');
+Route::view('/contacto', 'PaginaContacto')->name('contacto');
+Route::view('/unete', 'PaginaUnete')->name('unete');
+Route::view('/login', 'login')->name('login');
+Route::view('/configuracion', 'configuracion')->name('config');
